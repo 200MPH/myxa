4f30a132eedf7197d02960d5c8722689ea52d1fd
Wojtek <wojciech@geniusmoney.co.uk>
2026-04-12 19:47:18 +0200

    File permissions on boot - for Docker

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -eu
 PHP_CONFIG_DIR="/usr/local/etc/php/conf.d"
 TARGET_INI="${PHP_CONFIG_DIR}/zz-myxa.ini"
 APP_ENV="${APP_ENV:-local}"
+APP_ROOT="/var/www/html"
+STORAGE_DIR="${APP_ROOT}/storage"
 
 case "${APP_ENV}" in
     production|prod)
@@ -15,5 +17,10 @@ case "${APP_ENV}" in
 esac
 
 cat /usr/local/etc/php/myxa/php.common.ini "${ENV_INI}" > "${TARGET_INI}"
+
+if [ -d "${APP_ROOT}" ]; then
+    mkdir -p "${STORAGE_DIR}/data/logs"
+    chmod -R a+rwX "${STORAGE_DIR}" 2>/dev/null || true
+fi
 
 exec docker-php-entrypoint "$@"
